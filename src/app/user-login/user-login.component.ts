052bc5d9b783999c6cc7d42ad946fefc0ffbdd02
CarlosEduardodosSantos <carlos.santos@zipsoftware.com.br>
2022-03-25 15:17:10 -0300

    CartãoAlphaNumerico

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -7,90 +7,70 @@ import { NumberFormatStyle } from '@angular/common';
 import { Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { iMov } from '../iMov';
-import {login, loginC, senha, senhaC} from  '../app-routing.module'
-import {environment} from'../../environments/environment'
-
+import { login, loginC, senha, senhaC } from '../app-routing.module';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.scss'],
 })
 export class UserLoginComponent implements OnInit {
-
   consumo: any;
   consumId: any;
   login = login;
   loginC = loginC;
   senha = senha;
-  senhaC= senhaC;
-  isAdmin : any;
+  senhaC = senhaC;
+  isAdmin: any;
 
-
-  constructor(private CartaoConsumoService: CartaoConsumoService, private router: Router) { }
-
+  constructor(
+    private CartaoConsumoService: CartaoConsumoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.CartaoConsumoService.isAdmin)
-    this.CartaoConsumoService.token = this.generateToken()
+    console.log(this.CartaoConsumoService.isAdmin);
+    this.CartaoConsumoService.token = this.generateToken();
   }
 
-  close(){window.close(); }
+  close() {
+    window.close();
+  }
 
   async obterConsulByNr(consumId: any) {
-
-    this.CartaoConsumoService.obterConsuByNr(consumId).then(consum => { this.consumo = consum; console.log(consum);
-      if(this.consumo!== null)
-      {
+    this.CartaoConsumoService.obterConsuByNr(consumId).then((consum) => {
+      this.consumo = consum;
+      console.log(consum);
+      if (this.consumo !== null) {
         this.goToUser(consumId);
-      }
-      else
-      {
-        window.alert("Cartão consumo não encontrado!");
+      } else {
+        window.alert('Cartão consumo não encontrado!');
       }
     });
-
   }
 
-
-goToUser(consumId: any){
-  console.log(this.router.url)
-  this.router.navigate(['/user/'+ consumId])
-}
-
-goToAdmin(login: any, senha: any)
-{
-  if(login == this.login && senha == this.senha)
-  {
-    localStorage.setItem('admin', '1');
-    this.CartaoConsumoService.isAdmin = localStorage.getItem('admin');
-    console.log(this.CartaoConsumoService.isAdmin)
-    this.router.navigate(['/admin'])
+  goToUser(consumId: any) {
+    console.log(this.router.url);
+    this.router.navigate(['/user/' + consumId]);
   }
 
-  else if(login == this.loginC && senha == this.senhaC)
-  {
-    localStorage.setItem('admin', '3');
-    this.CartaoConsumoService.isAdmin = localStorage.getItem('admin');
-    this.router.navigate(['/admin'])
+  goToAdmin(login: any, senha: any) {
+    if (login == this.login && senha == this.senha) {
+      localStorage.setItem('admin', '1');
+      console.log(this.CartaoConsumoService.isAdmin);
+      this.router.navigate(['/admin']);
+    } else if (login == this.loginC && senha == this.senhaC) {
+      localStorage.setItem('admin', '3');
+      this.router.navigate(['/admin']);
+    } else {
+      localStorage.setItem('admin', '2');
+      this.isAdmin = localStorage.getItem('admin');
+      alert('Credenciais Incorretas!');
+    }
   }
-  else
-  {
-    localStorage.setItem('admin', '2');
-    this.isAdmin = localStorage.getItem('admin');
-    alert('Credenciais Incorretas!')
-    
+
+  generateToken() {
+    return Math.floor(Math.random() * (100000 + 1));
   }
-
-
 }
-
-generateToken()
-{
-  return Math.floor(Math.random() * (100000 + 1));
-}
-
-}
-
-
-
